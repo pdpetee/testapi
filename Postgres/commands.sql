a6089fae245c91644436facac0235a41d8548f9a
pdpetee <64540409+pdpetee@users.noreply.github.com>
2021-01-26 13:15:28 +0800

    Add files via upload

--- Postgres/commands.sql
+++ Postgres/commands.sql
@@ -42,33 +42,46 @@ $$ language plpgsql stable;
 
 
 
-
-create function createjwt(_session_token uuid, _user_id integer, _role text, out _jwt text) as $$
+create or replace function createjwt(_is_refresh_token boolean, _session_token uuid, _user_id integer, _role text, out _jwt text) as $$
 declare
 _tokenheader json;
+_expire text;
 	begin
-	select concat('{"session_token":"',_session_token,'","user_id":',_user_id,',"role":"', _role, '"}') into _tokenheader;
+	if not _is_refresh_token then
+	select (now()::timestamp + interval '1 minutes')::text into _expire;
+	end if;
+	if _is_refresh_token then
+	select (now()::timestamp + interval '1 day')::text into _expire;
+	end if;
+	select concat('{"session_token":"',_session_token,'","user_id":',_user_id,',"role":"', _role, '","expire":"', _expire,'"}') into _tokenheader;
 	select sign(_tokenheader, 'secretkey') into _jwt;
 	end;
 	$$ language plpgsql stable;
 
-
-
-create function verifyjwt(jwt text, out isTokenValid boolean) as $$
+create or replace function verifyjwt(jwt text, out is_token_valid boolean) as $$
+declare
+	is_secret_valid boolean;
+	json_body text;
+	_expiry_date text;
+	_current_date text;
+	_token text;
+	
 	begin
-	select valid from verify(jwt, 'secretkey') into isTokenValid;
+	select valid from verify(jwt, 'secretkey') into is_secret_valid;
+	if is_secret_valid then
+	select payload from verify(jwt, 'secretkey') into json_body;
+	select * from json_extract_path(json_body::json, 'expire') into _expiry_date;
+	select now()::timestamp into _current_date;
+	if _current_date::timestamp < _expiry_date::timestamp then
+	is_token_valid := true;
+	else
+	select json_body::json ->> 'session_token' into _token;
+	delete from session where token = _token::uuid;
+	is_token_valid := false;
+	end if;
+	end if;
 	end;
-	$$ language plpgsql stable;
-
-
-
-
-create function getRoleFromjwt(jwt text, out role text) as $$
-	begin
-	select valid from verify(jwt, 'secretkey') into role;
-	end;
-	$$ language plpgsql stable;
-
+	$$ language plpgsql volatile;
 
 
 create function login(_name text, _password text, out _jwt text) as $$
@@ -90,7 +103,24 @@ _token uuid;
 	end;
 $$ language plpgsql security definer;
 
-
+create function login(_is_refresh boolean, _name text, _password text, out _jwt text) as $$
+declare 
+_user users;
+_token uuid;
+	begin
+	select * from users where name = _name into _user;
+	if _user is null or
+	_password != _user.password
+	then
+		raise 'INVALID_LOGIN';
+	else
+		insert into session (user_id) values (_user.id)
+		returning token into _token;
+	perform setauth(_token::text);
+	select createjwt(_is_refresh, _token, _user.id, _user.role) into _jwt;
+	end if;
+	end;
+$$ language plpgsql security definer;
 
 create function login(inout _token uuid) as $$
 begin
@@ -98,7 +128,7 @@ perform setauth(NULL);
 perform token2user(_token);
 perform setauth(_token::text);
 end;
-$$ language plpgsql security definer;
+$$ language plpgsql security definer;a
 
 
 
@@ -110,3 +140,6 @@ exception when others then end;
 perform setauth(NULL);
 end;
 $$ language plpgsql security definer;
+
+
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uX3Rva2VuIjoiMmM0OGIzN2ItYTc5Ny00ZDBjLWExZmUtZjlhMWI1YTIyMzkxIiwidXNlcl9pZCI6MSwicm9sZSI6Im93bmVyIiwiZXhwaXJlIjoiMjAyMS0wMS0yNSAxNzo0OTowOC4wNzA1NDkifQ.VgdedseiFsxGRIwYt82_-pyAFI4_uYSsoU2_RqeQyRo
